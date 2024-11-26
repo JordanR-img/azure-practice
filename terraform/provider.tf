8498936e24a81c053a2dc375c9f3f0ea8e9580e7
jordan <jordanrobin18@gmail.com>
2024-11-26 23:37:51 +1300

    test

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,11 +11,9 @@ terraform {
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
       use_oidc             = true
-      use_azuread_auth     = true
   }
 }
 
 provider "azurerm" {
   features {}
-  use_oidc = true
 }
