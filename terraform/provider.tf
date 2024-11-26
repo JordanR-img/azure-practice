8c3f1746759705926a1732b8730aea37a3577a96
jordan <jordanrobin18@gmail.com>
2024-11-26 23:04:03 +1300

    adding necessary stuff

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,6 +12,7 @@ terraform {
       key                  = "terraform.tfstate"
       use_oidc             = true
       tenant_id            = "134721f3-68ee-4ce1-8951-57e0578ab41a"
+      client_id            = "2c350fbd-72d0-44db-96e6-4e411b771033"
   }
 }
 
